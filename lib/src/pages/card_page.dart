63afa805f4a52401be46e26db6339fe0877af2c6
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-24 09:20:08 +0100

    Adding static assets (Image placeholder)

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -11,37 +11,63 @@ class CardPage extends StatelessWidget {
         //padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
         ],
       ),
     );
   }
-}
 
-Widget _cardTipo1() {
-
-  return Card(
-    child: Column(
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.photo_album, color: Colors.blue,),
-          title: Text('Soy el título de esta tarjeta'),
-          subtitle: Text('Aquí estamos con la descripción de la tarjeta que quiero que ustedes vean para tener una idea de lo que quiero mostrarles'),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FlatButton(
-              child: Text('Cancelar'),
-              onPressed: () {},
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
             ),
-            FlatButton(
-              child: Text('Ok'),
-              onPressed: () {},
-            )
-          ],
-        ),
-      ],
-    ),
-  );
+            title: Text('Soy el título de esta tarjeta'),
+            subtitle: Text(
+                'Aquí estamos con la descripción de la tarjeta que quiero que ustedes vean para tener una idea de lo que quiero mostrarles'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          // FadeInImage(
+          //   placeholder: ,
+          // ),
+          // Image(
+          //   image: NetworkImage('https://cdn.cnn.com/cnnnext/dam/assets/190517091026-07-unusual-landscapes-travel.jpg'),
+          // ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('No sé qué poner')
+          )
+        ],
+      ),
+    );
+  }
 }
