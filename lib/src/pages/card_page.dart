77c676d3a40c13ebba996bccac4f0f23ccee995b
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-24 09:00:10 +0100

    First card and adding padding to listview

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -7,6 +7,26 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: ListView(
+        //padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
     );
   }
+}
+
+Widget _cardTipo1() {
+
+  return Card(
+    child: Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.photo_album, color: Colors.blue,),
+        )
+      ],
+    ),
+  );
 }
