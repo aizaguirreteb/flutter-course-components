6bce2feaa8e1234dba6fefd8c0e8a039d61ad87b
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-24 10:08:03 +0100

    Adding image and simple avatars to AppBar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,33 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/7/7b/Stan_Lee_by_Gage_Skidmore_3.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            /*
+            Para despegar el avatar del lado derecho lo metemos en un 
+            container y le damos la propiedad margin
+            */
+            margin: EdgeInsets.only(right: 10.0), 
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/original.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+          image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/7/7b/Stan_Lee_by_Gage_Skidmore_3.jpg'),
+        ),
       ),
     );
   }
