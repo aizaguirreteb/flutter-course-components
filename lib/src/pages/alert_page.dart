2fa812d15a63010e424860f46f87faacf0349f64
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-24 09:57:21 +0100

    Implementing alert dialog and its buttons

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,12 +8,55 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          onPressed: () => _mostrarAlerta(context),
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+
+      context: context,
+      barrierDismissible: true, //propiedad para cerrar la alerta al pulsar fuera de ella
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Título'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min, //para que la columna se adapte al contenido interno
+            children: <Widget>[
+              Text('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(size: 100,) //añadir logo de flutter
+            ],
+          ),
+          actions: <Widget>[ //Botones del alert dialog!!!
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
+
     );
   }
 }
