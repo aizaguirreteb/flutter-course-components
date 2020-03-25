f07690a67a8310f51af68cc3579f39fc31a3304e
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-25 10:15:59 +0100

    Datepicker and showing it in Spanish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
@@ -11,6 +12,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English
+        const Locale('es', 'ES'), // Spanish      
+      ],
       title: 'Componentes APP',
       // home:  HomePage(),
       initialRoute: '/',
--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -10,6 +10,8 @@ class _InputPageState extends State<InputPage> {
   String _nombre = '';
   String _email  = '';
   String _password = '';
+  String _fecha = '';
+  TextEditingController _inputFieldDateController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +27,8 @@ class _InputPageState extends State<InputPage> {
           _crearEmail(),
           Divider(),
           _crearPassword(),
+          Divider(),
+          _crearFecha(context),
           Divider(),
           _crearPersona(),          
         ],
@@ -106,5 +110,45 @@ class _InputPageState extends State<InputPage> {
         print(_nombre);
       },
     );
+  }
+
+  Widget _crearFecha(BuildContext context) {
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFieldDateController,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Fecha de nacimiento',
+        labelText: 'Fecha de nacimiento',
+        suffixIcon: Icon(Icons.perm_contact_calendar),
+        icon: Icon(Icons.calendar_today)
+      ),
+      onTap: () {
+
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+
+      },
+    );
+  }
+
+  _selectDate (BuildContext context) async {
+    
+    DateTime picked = await showDatePicker(
+      context: context, 
+      initialDate: new DateTime.now(), 
+      firstDate: new DateTime(2020), 
+      lastDate: new DateTime(2025),
+      locale: Locale('es', 'ES'),
+    );
+
+    if( picked != null){
+      setState(() {
+        _fecha = picked.toString();
+        _inputFieldDateController.text = _fecha;
+      });
+    }
   }
 }
