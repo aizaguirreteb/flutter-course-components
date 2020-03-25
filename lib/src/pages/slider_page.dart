8cabb18091ae5e572df160b030b9a3b7bf95768f
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-25 17:57:31 +0100

    Slider with image

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+
+  double _valorSlider = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sliders'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            Expanded(
+              child: _crearImagen()
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: 'Tamaño de la imagen',
+      //divisions: 20,
+      value: _valorSlider,
+      min: 0.0,
+      max: 400.0,
+      onChanged: (value) {
+        setState(() {
+          _valorSlider = value;
+        });
+      },
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage('https://banner2.cleanpng.com/20180405/azq/kisspng-kitten-havana-brown-meow-clip-art-animated-cats-5ac6078fc3b627.0233253615229275038016.jpg'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+}
