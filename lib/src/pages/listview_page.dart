701e6f000151d4bc73cbd784c971b1b1e91eb45e
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-26 09:29:02 +0100

    Loading and using ScrollController

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ListaPage extends StatefulWidget {
@@ -12,6 +14,7 @@ class _ListaPageState extends State<ListaPage> {
 
   List<int> _listaNumeros = new List();
   int _ultimoItem = 0;
+  bool _isLoading = false;
 
 
   @override
@@ -24,9 +27,16 @@ class _ListaPageState extends State<ListaPage> {
     _scrollController.addListener(() {
       print('Scroll!!!!!');
       if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
-        _agregar10();
+        // _agregar10();
+        fetchData();
       }
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 
   @override
@@ -35,7 +45,13 @@ class _ListaPageState extends State<ListaPage> {
       appBar: AppBar(
         title: Text('Listas'),
       ),
-      body: _crearLista(),
+      body: Stack(
+        children: <Widget>[
+          _crearLista(),
+          _crearLoading(),
+        ],
+      ),
+      
     );
   }
 
@@ -64,5 +80,48 @@ class _ListaPageState extends State<ListaPage> {
     setState(() {
       
     });
+  }
+
+  Future<Null> fetchData() async{
+    _isLoading = true;
+    setState(() { });
+
+    final duration = new Duration(seconds: 2);
+    return new Timer(duration, respuestaHTTP);
+    
+  }
+
+  void respuestaHTTP(){
+    _isLoading = false;
+
+    _scrollController.animateTo(
+        _scrollController.position.pixels + 100,
+        curve: Curves.fastOutSlowIn,
+        duration: Duration(milliseconds: 250)
+    );
+    _agregar10();
+  }
+
+  Widget _crearLoading() {
+
+    if(_isLoading){
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+            ],
+          ),
+          SizedBox(height: 15.0,),
+        ],
+      );
+      
+      
+    } else {
+      return Container();
+    }
   }
 }
